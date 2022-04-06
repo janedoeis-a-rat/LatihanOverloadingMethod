c9217e4c904dc8b31713ddbdc3171670b46417f6
Reksa Panduasa <panduasareksa@students.amikom.ac.id>
2022-04-07 04:52:41 +0700

    first commit

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        private int a, b;
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            a = number1 < number2 ? number1 : number2;
+            b = number2 < number3 ? number2 : (int)number3;
+            return number3 < number1 ? (int)number3 : number1;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        public int FindMaximum(int number1, int number2, int number3)
+        {
+            a = number1 > number2 ? number1 : number2;
+            b = number2 > number3 ? number2 : (int)number3;
+            return number3 > number1 ? (int)number3 : number1;
+        }
+    }
+}
